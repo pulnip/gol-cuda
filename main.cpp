d3f3bfd66d06a69fb343fc5f046c31dd57ef3404
pulnip <choiwh1008@gmail.com>
2025-08-02 22:58:52 +0900

    simple opengl test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,89 @@
+#include <stdio.h>
 #include <SDL3/SDL.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL_main.h>
+#include <glad/glad.h>
 #include "cuda_entry.h"
 
 struct AppState{
     SDL_Window* window;
+    SDL_GLContext context;
+
+    unsigned int vertexArray, vertexBuffer;
+    GLuint program;
 };
+
+GLuint loadShader(GLenum type, const char* path) {
+    // 1. 파일 열기
+    FILE* file = fopen(path, "rb");
+    if (!file) {
+        printf("Failed to open shader file: %s\n", path);
+        return 0;
+    }
+
+    // 2. 파일 크기 알아내고 버퍼 만들기
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* source = (char*)malloc(length + 1);
+    fread(source, 1, length, file);
+    source[length] = '\0'; // 널문자 추가!
+    fclose(file);
+
+    // 3. OpenGL에 셰이더 만들고 소스 등록
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, (const char**)&source, NULL);
+    glCompileShader(shader);
+    free(source);
+
+    // 4. 컴파일 에러 체크
+    GLint compiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled) {
+        GLint logLength = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+        if (logLength > 0) {
+            char* log = (char*)malloc(logLength);
+            glGetShaderInfoLog(shader, logLength, NULL, log);
+            printf("Shader compile error in %s:\n%s\n", path, log);
+            free(log);
+        }
+        glDeleteShader(shader);
+        return 0;
+    }
+
+    return shader;
+}
+
+GLuint createProgram(const char* vertPath, const char* fragPath){
+    GLuint vert = loadShader(GL_VERTEX_SHADER, vertPath);
+    GLuint frag = loadShader(GL_FRAGMENT_SHADER, fragPath);
+
+    GLuint prog = glCreateProgram();
+    glAttachShader(prog, vert);
+    glAttachShader(prog, frag);
+    glLinkProgram(prog);
+
+    GLint linked = 0;
+    glGetProgramiv(prog, GL_LINK_STATUS, &linked);
+    if(!linked){
+        GLint logLength = 0;
+        glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &logLength);
+        if (logLength > 0){
+            char* log = (char*)malloc(logLength);
+            glGetProgramInfoLog(prog, logLength, NULL, log);
+            printf("Program link error:\n%s\n", log);
+            free(log);
+        }
+        glDeleteProgram(prog);
+        return 0;
+    }
+
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+
+    return prog;
+}
 
 SDL_AppResult SDL_AppInit([[maybe_unused]] void** appState,
     [[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
@@ -29,15 +107,49 @@ SDL_AppResult SDL_AppInit([[maybe_unused]] void** appState,
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    SDL_WindowFlags flags = /* SDL_WINDOW_TRANSPARENT | SDL_WINDOW_BORDERLESS | */ SDL_WINDOW_OPENGL;
+    SDL_WindowFlags flags = SDL_WINDOW_TRANSPARENT | SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL;
 
     auto window = SDL_CreateWindow("GoL-CUDA", 800, 600, flags);
     if(window == nullptr){
         SDL_Log("Couldn't create window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+    auto context = SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        SDL_Log("Failed to initialize GLAD");
+        return SDL_APP_FAILURE;
+    }
+    SDL_GL_SetSwapInterval(1);
+
+    unsigned int vertexArray, vertexBuffer;
+    glGenVertexArrays(1, &vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+
+    glBindVertexArray(vertexArray);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    float lineVerts[] = {
+        -0.5f, -0.5f,
+         0.5f,  0.5f
+    };
+    glBufferData(GL_ARRAY_BUFFER, sizeof(lineVerts), lineVerts, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), (void*)0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    auto prog = createProgram("vertex.glsl", "fragment.glsl");
 
     app->window = window;
+    app->context = context;
+
+    app->vertexArray = vertexArray;
+    app->vertexBuffer = vertexBuffer;
+
+    app->program = prog;
+
     *appState = app;
 
     return SDL_APP_CONTINUE;
@@ -83,11 +195,19 @@ SDL_AppResult SDL_AppEvent([[maybe_unused]] void* appState,
 }
 
 SDL_AppResult SDL_AppIterate(void* appState){
+    auto app = static_cast<AppState*>(appState);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(app->program);
+    glBindVertexArray(app->vertexArray);
+    glDrawArrays(GL_LINES, 0, 2);
+
+    SDL_GL_SwapWindow(app->window);
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void* appState, [[maybe_unused]] SDL_AppResult result){
-    AppState* app = static_cast<AppState*>(appState);
+    auto app = static_cast<AppState*>(appState);
 
     SDL_DestroyWindow(app->window);
     delete app;
